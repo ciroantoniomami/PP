8280e4defa21bdc0c53879a3a4d0ee379ff9136a
ciroantonio <mamiciroantonio@gmail.com>
2020-12-07 12:32:23 +0100

    Update

--- Code/Jacobi2D.c
+++ Code/Jacobi2D.c
@@ -1,49 +1,109 @@
-#include<mpi.h>
-#include<stdio.h>
+#include <mpi.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
 
+int main( int argc , char* argv[] ){
 
-int main(int argc,char* argv[]){
+    MPI_Init( &argc , &argv );
+    MPI_Status status;
 
-    MPI_Init(&argc,&argv);
 
     int size;
-    MPI_Comm_size(MPI_COMM_WORLD,&size);
+    MPI_Comm_size( MPI_COMM_WORLD , &size );
 
-    int dims[2]={0,0};
-    MPI_Dims_create(size,2,dims);
+    int dims[2] = {0,0};
+    MPI_Dims_create( size ,2 , dims );
 
-    int periods[2]={true,true};
-    int reorder=true;
+    int periods[2] = { false , false };
+    int reorder = true;
 
     MPI_Comm new;
-    MPI_Cart_create(MPI_COMM_WORLD,2,dims,periods,reorder,&new);
+    MPI_Cart_create( MPI_COMM_WORLD ,2 , dims ,
+    periods , reorder , &new );
 
     int rank;
-    MPI_Comm_rank(new,&rank);
+    MPI_Comm_rank( new , &rank );
 
-    if(rank==0){
-        int N[2];
-        printf("Insert problem size on first axis:\n");
-        scanf("%d",&N[0]);
-        printf("Insert problem size on second axis:\n");
-        scanf("%d",&N[1]);
+    
+    int N[2];
+    //Ask for the dimension of the problem
+    if( rank == 0 ){
+        
+        printf( "Insert problem size on first axis:\n" );
+        scanf( "%d" , &N[0] );
+        printf( "Insert problem size on second axis:\n" );
+        scanf( "%d" , &N[1] );
+
+        
     }
 
-    MPI_Barrier(new);
+    MPI_Barrier( new );
+    MPI_Bcast( N ,2 ,MPI_INT ,0 , new );
     
-    int up,down,left,right;
-    MPI_Cart_shift(new,1,1,&up,&down);
-    MPI_Cart_shift(new,0,1,&left,&right);
+    
+    
+    
+    //Let's find the neighbours for each proc
+    
 
+    //Collect the coordinates
     int my_coord[2];
-    MPI_Cart_coords(new,rank,2,my_coord);
-    printf("[Rank: %d] coordinates (%d,%d) up: %d down: %d left: %d right: %d\n",rank,my_coord[0],my_coord[1],up,down,left,right);
+
+    MPI_Cart_coords( new , rank , 2 , my_coord );
+    
+
+    //Define the number of elements in each dimension for each proc
+    int local_dim[2];
+    
+    for( int i = 0; i < 2 ; i++ ){ 
+
+    local_dim[i] = N[i]/dims[i];
+    if( rank == (size-1)  )    local_dim[i] += N[i]%dims[i];
+
+    }
+    
+    int iEnd = local_dim[0] + 1;
+    int jEnd = local_dim[1] + 1;
+    
+    printf( "[Rank: %d] my dimensions are (%d,%d)\n" , rank , local_dim[0] , local_dim[1] );
+
+    double phi[iEnd][jEnd][2];
+
+    int MaxBufLen = max( local_dim[0] , local_dim[1] );
+
+    double fieldSend[MaxBufLen];
+    double fieldRecv[MaxBufLen];
+
+    int t0 = 0;
+    int t1 = 1;
+    double eps = 10^(-14);
+    double maxdelta = 10;
+    int iter;
+    int maxiter = 10000;
+    do while( maxdelta > eps && iter < maxiter ){
+
+        for( int dir = 0 ; dir < 2 ; dir++ ){
+            int source;
+            int dest;
+
+            MPI_Cart_shift( new , dir , 1 , &source , &dest );
+
+            if( source ){
+                MPI_Recv( fieldRecv , local_dim[dir] , MPI_DOUBLE , source , 0 , new , &status );
+            }
+
+            if ( dest ){
+                
+            }
+        }
 
 
-
-
+    }
 
     MPI_Finalize();
     return 0;
---
+++ Code/binary.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+
+
+#if defined(_OPENMP)
+#define CPU_TIME (clock_gettime( CLOCK_REALTIME, &ts ), (double)ts.tv_sec + \
+		  (double)ts.tv_nsec * 1e-9)
+
+#define CPU_TIME_th (clock_gettime( CLOCK_THREAD_CPUTIME_ID, &myts ), (double)myts.tv_sec +	\
+		     (double)myts.tv_nsec * 1e-9)
+
+#else
+
+#define CPU_TIME (clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &ts ), (double)ts.tv_sec + \
+		  (double)ts.tv_nsec * 1e-9)
+#endif
+
+int binarysearch( int *data , int start , int end , int Key );
+
+int main ( int argc , char * argv[] ){
+
+    long long int N;
+    struct  timespec ts;
+    double startTime , endTime ;
+    printf("Insert length array\n");
+    scanf("%lld" , &N );
+    
+    int *a = (int*)malloc(sizeof(int) * N) ;
+    for(int i = 0 ; i < N ; i++)
+        a[i] = i ;
+    
+    int key ;
+    printf("Insert key\n");
+    scanf("%d" , &key );
+
+    startTime = CPU_TIME;
+    #if defined(_OPENMP)
+
+    int nthreads = omp_get_num_threads() ;
+    long long int k = N/nthreads ;
+    int control = 0;
+
+    #pragma omp parallel
+    {
+        int id = omp_get_thread_num() ;
+        int found = -1 ;
+        long long int start = id * k ;
+        long long int end = start + k - 1 ;
+        found = binarysearch ( a , start , end , key ) ;
+        if (found != -1){
+                printf("found! %d \n" , found ) ;    
+                    
+                #pragma omp cancel parallel
+            }
+
+        #pragma omp cancellation point parallel
+
+            
+        
+            
+        #pragma omp single 
+        {
+                found = binarysearch ( a , k*nthreads , N-1 , key ) ;
+                if (found != -1)
+                {
+                    printf("found!\n") ;
+                    endTime = CPU_TIME ;
+
+                    printf("Walltime : %g\n" , endTime - startTime);
+                }
+
+        }
+    } 
+               
+           
+            
+    
+    
+        
+       
+       
+    
+
+    endTime = CPU_TIME ;
+
+    printf("Walltime : %g\n" , endTime - startTime);
+    #else
+    int found ;
+    found = binarysearch (a , 0 , N-1 , key);
+    if(found != -1)
+            printf("found! %d\n" , found);
+    
+    endTime = CPU_TIME ;
+
+    printf("Walltime : %g\n" , endTime - startTime);
+
+    #endif
+     
+    free(a) ;
+    return 0 ;
+
+}
+
+int binarysearch( int *data , int start , int end , int Key ){
+
+    int register low = start ;
+    int register high = end ;
+    int register mid ;
+    while ( low <= high ){
+        mid = ( low + high ) / 2 ;
+
+        if ( data[mid] < Key )
+            low = mid + 1 ;
+
+        else if (data[mid] > Key)
+            high = mid - 1 ;
+        
+        else
+            return mid ;
+        
+        
+    }
+    return -1 ;
+}
