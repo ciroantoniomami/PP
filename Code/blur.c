e234ffa7f7c10489c689ab8fd073fd897bd52d0e
ciroantonio <mamiciroantonio@gmail.com>
2020-12-15 19:15:02 +0100

    Update

--- Code/blur.c
+++ Code/blur.c
@@ -3,7 +3,7 @@
 #include <malloc.h>
 #include <stdbool.h>
 #include <stdlib.h>
-
+#define MPI_Proc_null -1 
 
 void kernel (double * K , int dim_kernel , int type_kernel , int weight) ;
 
@@ -33,6 +33,7 @@ int main (int argc , char * argv[]){
     int weight ;
     if (atoi (argv[4])) { weight = atoi (argv[4]) ;}
     else {weight = 0 ;}
+    int kdim = dim_kernel / 2 ;
 
     double * K ;
     K = (double*)malloc( dim_kernel * dim_kernel * sizeof(double)) ;
@@ -40,8 +41,8 @@ int main (int argc , char * argv[]){
 
     
     MPI_Init(&argc , &argv);
-    MPI_Status status;
-
+    MPI_Status status ;
+    MPI_Request request ;
     //Create a new comm world
     int size;
     MPI_Comm_size(MPI_COMM_WORLD , &size);
@@ -112,8 +113,79 @@ int main (int argc , char * argv[]){
         }
     }*/
 
+    // The structure that will be used to exchange data between   
+    short int * ubuffer = (short int *) calloc ( kdim * local_dim[0] , sizeof ( short int)) ;    
+    short int * dbuffer = (short int *) calloc ( kdim * local_dim[0] , sizeof ( short int)) ;
+    short int * lbuffer = (short int *) calloc ( local_dim[1] * kdim , sizeof ( short int)) ; 
+    short int * rbuffer = (short int *) calloc ( local_dim[1] * kdim , sizeof ( short int)) ; 
+    
+    int subsizes_col[2] = {kdim , local_dim[1]} ;
+    int subsizes_row[2] = {local_dim[0] , kdim} ;
+
+    MPI_Datatype type_col , colhalo, type_row , rowhalo ;
+
+    MPI_Type_create_subarray (2, local_dim , subsizes_col , starts , MPI_ORDER_C , MPI_SHORT , &type_col) ;
+    MPI_Type_create_resized (type_col, 0, kdim * sizeof(short int), &colhalo) ;
+    MPI_Type_commit (&colhalo) ;
+
+    MPI_Type_create_subarray (2, local_dim , subsizes_row , starts , MPI_ORDER_C , MPI_SHORT , &type_row) ;
+    MPI_Type_create_resized (type_row, 0, local_dim[0] * sizeof(short int), &rowhalo) ;
+    MPI_Type_commit (&rowhalo) ;
+
+    int source , dest ;
+
+    for (int disp = -1 ; disp < 2 ; disp = disp + 2){
+        for (int dir = 0 ; dir < 2 ; dir++){
+            MPI_Cart_shift (new , dir , disp , &source , &dest) ;
+
+            if (source != MPI_PROC_NULL && disp == -1 && dir == 0){
+                MPI_Irecv (rbuffer , 1 , colhalo , source , 123 , new , &request) ; 
+            }
+
+            if (source != MPI_PROC_NULL && disp == 1 && dir == 0){
+                MPI_Irecv (lbuffer , 1 , colhalo , source , 123 , new , &request) ;
+            }
+
+            if (source != MPI_PROC_NULL && disp == -1 && dir == 1){
+                MPI_Irecv (dbuffer , 1 , rowhalo , source , 123 , new , &request) ;
+            }
+
+            if (source != MPI_PROC_NULL && disp == 1 && dir == 1){
+                MPI_Irecv (ubuffer , 1 , rowhalo , source , 123 , new , &request) ;
+            }
+
+            if (dest != MPI_PROC_NULL && disp == -1 && dir == 0){
+                MPI_Send (local_M , 1 , colhalo , dest , 123 , new ) ;
+            }
+
+            if (dest != MPI_PROC_NULL && disp == 1 && dir == 0){
+                MPI_Send (&local_M[local_dim[0] - kdim] , 1 , colhalo , dest , 123 , new ) ;
+            }
+
+            if (dest != MPI_PROC_NULL && disp == -1 && dir == 1){
+                MPI_Send (local_M , 1 , rowhalo , dest , 123 , new ) ;
+            }
+
+            if (dest != MPI_PROC_NULL && disp == 1 && dir == 1){
+                MPI_Send (&local_M[(local_dim[0] * local_dim[1] - kdim * local_dim[0])] , 1 , rowhalo , dest , 123 , new ) ;
+            }
+            MPI_Wait( &request , &status) ;
+        }
+    }
+
+
+
+    MPI_Type_free (&type_col) ;
+    MPI_Type_free (&colhalo) ;
+    MPI_Type_free (&type_row) ;
+    MPI_Type_free (&rowhalo) ;
     MPI_Type_free (&resizedtype) ;
     free (local_M) ;
+    free (ubuffer) ;
+    free (dbuffer) ;
+    free (lbuffer) ;
+    free (rbuffer) ;
+
     MPI_Finalize () ;
 
     free ( M ) ;
